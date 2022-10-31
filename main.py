cb94c0b1780223bafb02c53def1022ce09bb65eb
Bartek <bartasx@hotmail.com>
2022-10-31 15:35:35 +0100

    Testin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#pylint:disable=R0201
-#pylint:disable=W0106
 import urllib.request
 from bs4 import BeautifulSoup
-
+import re
 
 class Definition:
 	URL = 'https://sjp.pwn.pl/szukaj/'
@@ -20,20 +18,39 @@ class Definition:
 		with urllib.request.urlopen(url) as web:
 			html = web.read()
 		return html
-		
+
 	def __read_data(self, web_data):
-		soup = BeautifulSoup(web_data,  'html.parser')
-		soup = soup.find_all(class_='entry')
-		dictionary = {}
-		for entry in soup:
-			if meaning:= entry.find_all(class_ = 'znacz'):
-				title = entry.find(class_ = 'tytul')
-				dictionary[title.text] = [:element.text.strip() for element in meaning]
+		dictionary = []
+		soup = BeautifulSoup(web_data, 'html.parser')
+		soup = soup.find(class_='wyniki sjp-so-wyniki sjp-so-anchor')
+		soup = soup.find(class_='ribbon-element')
+		title_tag = soup.find(class_='tytul')
+		title = self.__strip(title_tag)
+		title_tag.decompose()
+		other = self.__strip(soup)
+		return title, other
+
+	def read_data(self, web_data):
+		dictionary = []
+		soup = BeautifulSoup(web_data, 'html.parser')
+		soup = soup.find(class_='wyniki sjp-wyniki sjp-anchor')
+		soup = soup.find(class_='ribbon-element')
+		soup.span.decompose()
+		if meaning := soup.find_all(class_='znacz'):
+			dictionary = [self.__strip(element) for element in meaning]
+		else:
+			dictionary = [self.__strip(soup)]
 		return dictionary
+
+	def __strip(self, tag):
+		text = tag.get_text(strip=True)
+		start = text.find('«')
+		end = text.find('»')
+		text = text.replace('«', ' ').replace('»', '.').strip()
+		return text
 		
 
 if __name__ == '__main__':
 	d = Definition()
-	test = d.search('test')
+	test = d.search('defenestracja')
 	print(test)
-	
